4ed3be90238b719cbd6344420a60f6f58b3dea54
Azka <a.azkanorouzi@gmail.com>
2022-09-17 09:37:29 +0430

    add getUserSelection

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const ROCK_PAPER_SCISSOR = ['✂️', '📃', '🪨']
-
+/* Store user and computer score */
+let computerScore = 0;
+let userScore = 0;
+/* Getting the user choice */
+function getUserChoice() {
+  /* Store user's selection */
+  let playerSelection;
+  /* This loop will continue until user choose rock scissor or paper*/
+  do {
+    playerSelection = prompt('Choose one -> ✂️ 📃 🪨')
+  } while (playerSelection !== '✂️' && playerSelection !== '📃' && playerSelection !== '🪨')
+  /* Returning the user's selection */
+  return playerSelection;
+}
 /* Computer choice */
 function getComputerChoice() {
   // Generates a number between 0 and 2 (ROCK_PAPER_SCISSOR is zero base)
